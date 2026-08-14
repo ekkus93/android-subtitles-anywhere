864dfa3efc9fc426c18a772e3406e02a6d46294a
Phillip Chin <ekkus93@gmail.com>
2026-08-14 16:22:30 -0700

    SC-004: add Android baseline unit test

---
+++ android/app/src/test/java/com/ekkus93/silentcaption/BootstrapTest.kt
@@ -0,0 +1,11 @@
+package com.ekkus93.silentcaption
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class BootstrapTest {
+    @Test
+    fun baselineTestInfrastructureRuns() {
+        assertEquals("Silent Caption", "Silent Caption")
+    }
+}
