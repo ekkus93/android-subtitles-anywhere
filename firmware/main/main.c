f57d646181528be06998fcc051bafa03d35700e8
Phillip Chin <ekkus93@gmail.com>
2026-08-15 02:46:42 -0700

    SC-061-SC-066: start silent A2DP sink

--- firmware/main/main.c
+++ firmware/main/main.c
@@ -1,3 +1,4 @@
+#include "a2dp_sink.h"
 #include "esp_log.h"
 
 static const char *TAG = "silent_caption";
@@ -5,4 +6,5 @@ static const char *TAG = "silent_caption";
 void app_main(void)
 {
     ESP_LOGI(TAG, "Silent Caption firmware bootstrap");
+    sc_a2dp_sink_init();
 }
