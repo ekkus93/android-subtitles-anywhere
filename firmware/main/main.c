9dcffdcb300ec596896ec51172f5af9a4e5e01f2
Phillip Chin <ekkus93@gmail.com>
2026-08-15 03:33:03 -0700

    SC-101: initialize UART transport

--- firmware/main/main.c
+++ firmware/main/main.c
@@ -1,10 +1,12 @@
 #include "a2dp_sink.h"
 #include "esp_log.h"
+#include "uart_transport.h"
 
 static const char *TAG = "silent_caption";
 
 void app_main(void)
 {
     ESP_LOGI(TAG, "Silent Caption firmware bootstrap");
+    sc_uart_transport_init();
     sc_a2dp_sink_init();
 }
