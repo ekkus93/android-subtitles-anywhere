09997cc31b74e3adbd8719274385901783694e9c
Phillip Chin <ekkus93@gmail.com>
2026-08-16 11:49:38 -0700

    SC-G19: wire display modes to overlay lifecycle

---
+++ android/app/src/main/java/com/ekkus93/silentcaption/overlay/CaptionOverlayController.kt
@@ -0,0 +1,30 @@
+package com.ekkus93.silentcaption.overlay
+
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import com.ekkus93.silentcaption.ui.home.CaptionDisplayMode
+
+object CaptionOverlayController {
+    fun applyDisplayMode(
+        context: Context,
+        mode: CaptionDisplayMode,
+    ) {
+        val intent = Intent(context, CaptionOverlayService::class.java)
+        if (mode == CaptionDisplayMode.Reader) {
+            intent.action = CaptionOverlayService.ACTION_STOP
+            context.startService(intent)
+            return
+        }
+        intent.action = CaptionOverlayService.ACTION_SHOW
+        intent.putExtra(
+            CaptionOverlayService.EXTRA_MODE,
+            if (mode == CaptionDisplayMode.Compact) CaptionOverlayService.MODE_COMPACT else "floating",
+        )
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            context.startForegroundService(intent)
+        } else {
+            context.startService(intent)
+        }
+    }
+}
